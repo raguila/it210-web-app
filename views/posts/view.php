97a03be1eb24ee2417b96d35c8f2f79c5b3360ef
Mon <reactor04@gmail.com>
2014-12-05 21:44:18 +0800

    Removed Yii's default View Widgets. Comments presentation is now done manually with a foreach loop. Comments are now filtered for individual posts. Only the creator of the post will see the 'Update' and 'Delete' options.

--- views/posts/view.php
+++ views/posts/view.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\DetailView;
 use yii\widgets\ActiveForm;
 use app\models\Comments;
+use app\models\Users;
 use yii\grid\GridView;
 
 /* @var $this yii\web\View */
@@ -16,7 +17,20 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="posts-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
+    <div class="postDetails">
+        by <b><?php
+                $tempUserID = $model->UserID;
+                $tempUser = Users::findAll(['UserID' => $tempUserID]);
+                echo ($tempUser[0]->UserName);  ?></b> 
+        on <?php echo $model->TimeStamp ?>
+    </div>
+    <br/>
+    <div class="postContent">
+        <p><?= $model->PostContent ?></p>
+    </div>
+    <br/>
 
+    <?php if ($model->UserID == Yii::$app->user->identity->UserID) { ?>
     <p>
         <?= Html::a('Update', ['update', 'id' => $model->PostID], ['class' => 'btn btn-primary']) ?>
         <?= Html::a('Delete', ['delete', 'id' => $model->PostID], [
@@ -28,57 +42,49 @@ $this->params['breadcrumbs'][] = $this->title;
         ]) ?>
     </p>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'PostID',
-            'PostTitle',
-            'PostTypeID',
-            'PostContent',
-            'Tags',
-            'Attachment',
-            'AttachmentTypeID',
-            'UserID',
-            'Likes',
-            'Pinned',
-            'TimeStamp',
-        ],
-    ]) ?>
+    <?php } ?>
 
-    <br/><br/>
-    <h3>Add a comment</h3>
-    <?php
-        $form = ActiveForm::begin(); ?>
-
-        <?= $form->field($newComment, 'CommentContent')->textInput(['maxlength' => 255]) ?>
-
-        <?= $form->field($newComment, 'Attachment')->textInput(['maxlength' => 50, 'value' => 'NA']) ?>
-
-        <?= $form->field($newComment, 'AttachmentTypeID')->textInput(['value'=>1]) ?>
-
-        <?= $form->field($newComment, 'UserID',['template' => "{input}",])->hiddenInput(['value'=>Yii::$app->user->identity->UserID]) ?>
-
-        <?= $form->field($newComment, 'PostID',['template' => "{input}",])->hiddenInput(['value'=>$model->PostID]) ?>
+    
+    <div class="commentsOnPost">
+        <h4>Comments</h4>
+        <?php
+            foreach ($comments as $c): ?>
+            <ul class="list-group">
+                <li class="list-group-item">
+                    <b><?php
+                    $tempUserID = $c->UserID;
+                    $tempUser = Users::findAll(['UserID' => $tempUserID]);
+                    echo ($tempUser[0]->UserName); ?></b>
+                    <br/>
+                    <?php echo ($c->CommentContent); ?>
+                </li>
+            </ul>
+        <?php endforeach; ?>
         
-        <?= $form->field($newComment, 'Like', ['template' => "{input}"])->hiddenInput(['value'=>0]) ?>
+    </div>
 
-        <div class="form-group">
-            <?= Html::submitButton('Post', ['class' => 'btn btn-success']) ?>
-        </div>
-        
-    <?php ActiveForm::end(); ?>
 
-    <br/><br/>
-    <?= GridView::widget([
-        'dataProvider' => $comments,
-        //'filterModel' => $searchModel,
-        'columns' => [
-            //['class' => 'yii\grid\SerialColumn'],
+    <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9">
+        <h3>Add a comment</h3>
+        <?php
+            $form = ActiveForm::begin(); ?>
 
-            'CommentContent',
-            'TimeStamp',
+            <?= $form->field($newComment, 'CommentContent', ['template' => "{input}"])->textArea(['maxlength' => 255, 'rows' => 4]) ?>
 
-            ['class' => 'yii\grid\ActionColumn', ],
-        ],
-    ]); ?>
+            <?= $form->field($newComment, 'Attachment')->textInput(['maxlength' => 50, 'value' => 'NA']) ?>
+
+            <?= $form->field($newComment, 'AttachmentTypeID')->textInput(['value'=>1]) ?>
+
+            <?= $form->field($newComment, 'UserID',['template' => "{input}",])->hiddenInput(['value'=>Yii::$app->user->identity->UserID]) ?>
+
+            <?= $form->field($newComment, 'PostID',['template' => "{input}",])->hiddenInput(['value'=>$model->PostID]) ?>
+            
+            <?= $form->field($newComment, 'Like', ['template' => "{input}"])->hiddenInput(['value'=>0]) ?>
+
+            <div class="form-group">
+                <?= Html::submitButton('Post', ['class' => 'btn btn-success']) ?>
+            </div>
+            
+        <?php ActiveForm::end(); ?>
+    </div>
 </div>
